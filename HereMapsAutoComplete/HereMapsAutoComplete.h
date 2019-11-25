29a543f871200e064fcb63560bf3d0cb0a9758bb
rajanikaparthy <rajani@passenger.se>
2019-11-25 19:23:25 +0100

    Initial project setup

---
+++ HereMapsAutoComplete/HereMapsAutoComplete.h
@@ -0,0 +1,19 @@
+//
+//  HereMapsAutoComplete.h
+//  HereMapsAutoComplete
+//
+//  Created by Rajani Kaparthy on 2019-11-25.
+//  Copyright © 2019 dailyblah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HereMapsAutoComplete.
+FOUNDATION_EXPORT double HereMapsAutoCompleteVersionNumber;
+
+//! Project version string for HereMapsAutoComplete.
+FOUNDATION_EXPORT const unsigned char HereMapsAutoCompleteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HereMapsAutoComplete/PublicHeader.h>
+
+
---
+++ HereMapsAutoCompleteExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HereMapsAutoCompleteExample
+//
+//  Created by Rajani Kaparthy on 2019-11-25.
+//  Copyright © 2019 dailyblah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+     override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
